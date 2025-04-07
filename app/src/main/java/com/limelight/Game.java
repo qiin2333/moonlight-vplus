6c12e0489d19e665e304b839a4cde5999c7080c8
Xmqor <10818503+xmqor@user.noreply.gitee.com>
2025-04-08 00:37:59 +0800

    可移动按键增加手柄瞄准,不兼容旧版布局的可移动按键， 会闪退，或者提前把布局中的可移动按键删除，更新app 后再重新添加即可 建议以后加载按键时使用try 摇杆改为相对移动模式但未增加开关 希望相对移动模式和绝对位置增加开关(我没能抽出时间添加)

--- app/src/main/java/com/limelight/Game.java
+++ app/src/main/java/com/limelight/Game.java
@@ -3148,4 +3148,12 @@ public class Game extends Activity implements SurfaceHolder.Callback,
             setupDisplayPosition();
         }
     }
+
+    public StreamView getStreamView() {
+        return streamView;
+    }
+
+    public boolean getHandleMotionEvent(StreamView streamView,MotionEvent event) {
+        return handleMotionEvent(streamView,event);
+    }
 }
